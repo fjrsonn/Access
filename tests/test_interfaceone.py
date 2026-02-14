a407ef19994b520fc7ec3ecbc3bd5c4bf8686a19
Flavio Junior <fjrsonn@gmail.com>
2026-02-14 05:11:01 -0300

    Route encomenda by tipo/empresa/identificacao signals

--- tests/test_interfaceone.py
+++ tests/test_interfaceone.py
@@ -24,6 +24,22 @@ class InterfaceOneTests(unittest.TestCase):
         txt = "APT111 88SG4RSHNA8BR ENV RIACHUELO BLO13 JOAO PEREIRA"
         self.assertTrue(interfaceone._is_encomenda_text(txt, parsed={}))
 
+    def test_encomenda_alias_shop_detected(self):
+        txt = "EVELOPE SHOP BRUNO RIBEIRO BLOCO13 APARTAMEN109"
+        self.assertTrue(interfaceone._is_encomenda_text(txt, parsed={}))
+
+    def test_encomenda_when_only_tipo_signal(self):
+        txt = "ENVELOPE JOAO SILVA BLOCO 1 AP 22"
+        self.assertTrue(interfaceone._is_encomenda_text(txt, parsed={}))
+
+    def test_encomenda_when_only_loja_signal(self):
+        txt = "SHOPEE JOAO SILVA BLOCO 1 AP 22"
+        self.assertTrue(interfaceone._is_encomenda_text(txt, parsed={}))
+
+    def test_encomenda_when_only_identificacao_signal(self):
+        txt = "OSIJEVXTKTOTI JOAO SILVA BLOCO 1 AP 22"
+        self.assertTrue(interfaceone._is_encomenda_text(txt, parsed={}))
+
     def test_non_encomenda_person_record_not_forced(self):
         txt = "JOAO PEREIRA BLOCO 13 AP 111"
         self.assertFalse(interfaceone._is_encomenda_text(txt, parsed={}))
